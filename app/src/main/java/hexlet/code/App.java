3806a2ceab2107c0162b5766c0ebb5f4e25b8b18
Anastasiia Smirnova <nyakubtsova@gmail.com>
2023-09-18 23:34:20 +0300

    add makefile

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,7 +1,28 @@
 package hexlet.code;
 
-public class App {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
+
+import java.util.concurrent.Callable;
+
+@Command(name = "dendiff", mixinStandardHelpOptions = true,
+        description = "Compares two configuration files and shows a difference.")
+class App implements Callable<Integer> {
+
+    @Option(names = {"--help", "-h"}, usageHelp = true, description = "Show this help message and exit.")
+    boolean usageHelpRequested;
+
+    @Option(names = {"--version", "-V"}, versionHelp = true, description = "Print version information and exit.")
+    boolean versionInfoRequested;
+
+    @Override
+    public Integer call() throws Exception {
+        return null;
+    }
+
+    public static void main(String... args) {
+        int exitCode = new CommandLine(new App()).execute(args);
+        System.exit(exitCode);
     }
 }
