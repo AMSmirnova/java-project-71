2dfeb68f5eb97ddc33f2557047e800e2f53668e2
Anastasiia Smirnova <nyakubtsova@gmail.com>
2023-09-19 23:41:43 +0300

    fix differ

--- app/src/main/java/hexlet/code/Differ.java
+++ app/src/main/java/hexlet/code/Differ.java
@@ -7,6 +7,7 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.*;
+import java.util.stream.Stream;
 
 public class Differ {
     public static String generate(Path filepath1, Path filepath2) throws IOException {
@@ -23,22 +24,22 @@ public class Differ {
         return result.toString();
     }
 
-    public static Map<Map<String, Object>, String> genDiff(Map<String, Object> data1, Map<String, Object> data2) {
+    public static Map genDiff(Map<String, Object> data1, Map<String, Object> data2) {
 
-        Map<Map<String, Object>, String> result = new LinkedHashMap<>();
+        Map<String, String> result = new LinkedHashMap<>();
         Set<String> keys = new TreeSet<>(data1.keySet());
         keys.addAll(data2.keySet());
 
         for (String key: keys) {
             if (!data1.containsKey(key)) {
-                result.put(Map.of(key, data2.get(key)), " + ");
+                result.put(key + ": " + data2.get(key), " + ");
             } else if (!data2.containsKey(key)) {
-                result.put(Map.of(key, data1.get(key)), " - ");
+                result.put(key + ": " + data1.get(key), " - ");
             } else if (data1.get(key).equals(data2.get(key))) {
-                result.put(Map.of(key, data1.get(key)), "   ");
+                result.put(key + ": " + data1.get(key), "   ");
             } else {
-                result.put(Map.of(key, data1.get(key)), " - ");
-                result.put(Map.of(key, data2.get(key)), " + ");
+                result.put(key + ": " + data1.get(key), " - ");
+                result.put(key + ": " + data2.get(key), " + ");
             }
         }
         return result;
