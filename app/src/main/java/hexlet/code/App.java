5f3bd718d969d00d7101e869e239c892d4fec346
Anastasiia Smirnova <nyakubtsova@gmail.com>
2023-09-19 16:53:58 +0300

    add differ.java

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -6,6 +6,9 @@ import picocli.CommandLine.Option;
 import picocli.CommandLine.Parameters;
 
 import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.concurrent.Callable;
 
 @Command(name = "gendiff", mixinStandardHelpOptions = true,
@@ -20,6 +23,20 @@ class App implements Callable<Integer> {
     String format;
     @Override
     public Integer call() throws Exception {
+        Path path1 = Paths.get(filepath1).toAbsolutePath().normalize();
+        Path path2 = Paths.get(filepath2).toAbsolutePath().normalize();
+        // Проверяем существование файлов
+        if (!Files.exists(path1)) {
+            throw new Exception("File '" + path1 + "' does not exist");
+        }
+        if (!Files.exists(path2)) {
+            throw new Exception("File '" + path2 + "' does not exist");
+        }
+
+        var diff = Differ.generate(path1, path2);
+        System.out.println(diff);
+//        System.out.println(Files.readString(path1));
+
         return null;
     }
 
--- app/src/main/java/hexlet/code/Differ.java
+++ app/src/main/java/hexlet/code/Differ.java
@@ -1,8 +1,59 @@
 package hexlet.code;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeSet;
+import java.util.stream.Stream;
+
 public class Differ {
-    public static String generate() {
-        return "";
+    public static String generate(Path filepath1, Path filepath2) throws IOException {
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        Map<String, Object> mapFile1
+                = objectMapper.readValue(Files.readString(filepath1), new TypeReference<Map<String,Object>>(){});
+
+        Map<String, Object> mapFile2
+                = objectMapper.readValue(Files.readString(filepath2), new TypeReference<Map<String,Object>>(){});
+
+        var result = genDiff(mapFile1, mapFile2);
+
+
+        System.out.println(result);
+
+
+        return "hi";
+    }
+
+    public static Map<Map<String, Object>, String> genDiff(Map<String, Object> data1, Map<String, Object> data2) {
+
+        Map<Map<String, Object>, String> result = new LinkedHashMap<>();
+        Set<String> keys = new TreeSet<>(data1.keySet());
+        keys.addAll(data2.keySet());
+
+        for (String key: keys) {
+
+            if (!data1.containsKey(key)) {
+                result.put(Map.of(key, data2.get(key)), " + ");
+            } else if (!data2.containsKey(key)) {
+                result.put(Map.of(key, data1.get(key)), " - ");
+            } else if (data1.get(key).equals(data2.get(key))) {
+                result.put(Map.of(key, data1.get(key)), "   ");
+            } else {
+                result.put(Map.of(key, data1.get(key)), " - ");
+                result.put(Map.of(key, data2.get(key)), " + ");
+            }
+        }
+
+        return result;
     }
 }
 
